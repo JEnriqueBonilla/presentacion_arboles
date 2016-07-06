f8d323e6d649534243ff8a028f0aa32ecb8894af
kaelhuerta <kael.huerta@toptal.com>
2016-07-05 20:46:50 -0500

    agregado shiny varianza vs sesgo

---
+++ shiny/sesgo_vs_varianza/server.R
@@ -0,0 +1,52 @@
+#--------------------------------------------------------#
+#   Árboles de Decisión, Bosques Aleatorios y Boosting   #
+#   Preparado por:       Kael Huerta y Enrique Bonilla   #
+#                                  06 de Julio de 2016   #
+#--------------------------------------------------------#
+
+library(shinydashboard)
+library(data.table)
+library(dplyr)
+library(ggplot2)
+theme_set(theme_bw())
+
+real <- data.table(x = seq(0, 1, .01)) %>%
+  mutate(y = sin(2 * pi * x))
+
+shinyServer(function(input, output){
+  r <- reactive({
+    set.seed(3)
+    res <- data.table(x = seq(0, 1, 1 / input$n)) %>%
+      mutate(y = sin(2 * pi * x) + rnorm(length(x), 0, 0.5))
+    return(res)
+  })
+
+  output$plot <- renderPlot({
+    pl <- r() %>% ggplot(aes(x = x, y = y))
+
+    ## Simulaciones
+    if(input$simul)
+      pl <- pl + geom_point(colour = '#56B4E9', size = 2)
+
+    ## Modelo real
+    if(input$real)
+      pl <- pl + geom_line(data = real,
+        aes(x = x, y = y), colour = '#E69F00', alpha = .3, size = 1.5)
+
+    ## Aproximación polinomial
+    if(input$polinomio)
+      pl <- pl + geom_smooth(method = 'lm', formula = y ~ poly(x, input$k),
+        colour = 'salmon', alpha = .1, se = F)
+
+    if(input$simul | input$real | input$polinomio)
+      return(pl)
+    else
+      return(NULL)
+  })
+
+  output$table <- renderTable({
+    r()
+  })
+
+})
+
---
+++ shiny/sesgo_vs_varianza/ui.R
@@ -0,0 +1,90 @@
+#--------------------------------------------------------#
+#   Árboles de Decisión, Bosques Aleatorios y Boosting   #
+#   Preparado por:       Kael Huerta y Enrique Bonilla   #
+#                                  06 de Julio de 2016   #
+#--------------------------------------------------------#
+
+library(shinydashboard)
+
+sidebar <- dashboardSidebar(
+  hr(),
+  sidebarMenu(id="tabs",
+    menuItem("Teoría", tabName = "readme", icon = icon("mortar-board"), selected = T),
+    menuItem("Gráfica", tabName = "plot", icon = icon("line-chart")),
+    menuItem("Datos", tabName = "table", icon = icon("table")),
+    menuItem("Códigos",  icon = icon("file-text-o"),
+      menuSubItem("ui.R", tabName = "ui", icon = icon("angle-right")),
+      menuSubItem("server.R", tabName = "server", icon = icon("angle-right"))
+    ),
+    menuItem("Acerca de...", tabName = "about", icon = icon("question"))
+  ),
+  hr(),
+  conditionalPanel("input.tabs=='plot'",
+    fluidRow(
+      column(1),
+      column(10,
+        checkboxInput("real", "Modelo real", T),
+        checkboxInput("simul", "Datos simulados", F),
+        checkboxInput("polinomio", "Aproximación polinomial", F),
+        checkboxInput("arbol", "Árboles", F)
+      )
+    )
+  )
+)
+
+body <- dashboardBody(
+  tabItems(
+    tabItem(tabName = "readme",
+      withMathJax(),
+      includeMarkdown("teoria.Rmd")
+    ),
+    tabItem(tabName = "plot",
+      fluidRow(
+        column(width = 4,
+          tabBox( width = NULL,
+            tabPanel(h5("Parámetros"),
+              sliderInput("n", "Número de Simulaciones:", value = 11, min = 1, max = 25, step = 5),
+              sliderInput("k", "Complejidad:", value = 1, min = 1, max = 10, step = 1)
+            ),
+            tabPanel(h5("Modelo"),
+              includeMarkdown("formula.Rmd")
+            )
+          )),
+            column(width = 8,
+              box(  width = NULL, plotOutput("plot",height="500px"), collapsible = TRUE,
+                title = "Varianza vs. Sesgo", status = "primary", solidHeader = TRUE)
+          ))
+    ),
+    tabItem(tabName = "table",
+      box( width = NULL, status = "primary", solidHeader = TRUE, title="Table",
+        downloadButton('downloadTable', 'Download'),
+        br(),br(),
+        tableOutput("table")
+      )
+    ),
+    tabItem(tabName = "ui",
+      box( width = NULL, status = "primary", solidHeader = TRUE, title="ui.R",
+        downloadButton('downloadData2', 'Download'),
+        br(),br(),
+        pre(includeText("ui.R"))
+      )
+    ),
+    tabItem(tabName = "server",
+      box( width = NULL, status = "primary", solidHeader = TRUE, title="server.R",
+        downloadButton('downloadData3', 'Download'),
+        br(),br(),
+        pre(includeText("server.R"))
+      )
+    ),
+    tabItem(tabName = "about",
+      includeMarkdown("acerca.Rmd")
+    )
+  )
+)
+
+dashboardPage(
+  dashboardHeader(title = "Bosques Aleatorios"),
+  sidebar,
+  body
+)
+
